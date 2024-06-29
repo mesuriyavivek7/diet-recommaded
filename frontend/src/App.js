ab98d3d1146ee3cf4524a319a87fda55916403bc
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2024-06-29 09:38:07 +0530

    new-added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 
+import HomeSet from './pages/home/HomeSet';
+
 function App() {
   return (
     <div className="App">
-     
+        <HomeSet></HomeSet>
     </div>
   );
 }
