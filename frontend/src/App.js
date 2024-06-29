c329dca6b04f5c096d06b47228aaee21f6746e9c
Harshilshah11 <shahharshil054@gmail.com>
2024-06-29 10:52:01 +0530

    tailwind configured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 
 import HomeSet from './pages/home/HomeSet';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-        <HomeSet></HomeSet>
+        {/* <HomeSet></HomeSet> */}
+        <Footer/>
     </div>
   );
 }
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto flex items-center justify-between px-4">
+          <p>&copy; 2024 Your Company. All rights reserved.</p>
+          <div>
+            <a href="#" className="text-gray-300 hover:text-white mx-2">Home</a>
+            <a href="#" className="text-gray-300 hover:text-white mx-2">About</a>
+            <a href="#" className="text-gray-300 hover:text-white mx-2">Contact</a>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
