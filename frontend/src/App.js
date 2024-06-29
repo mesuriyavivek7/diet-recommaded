e79618a85b2616e8f98ed95e9f29b386ef662509
Harshilshah11 <shahharshil054@gmail.com>
2024-06-29 16:28:06 +0530

    UserProfile Added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 
-import './App.css';
-
 import HomeSet from './pages/home/HomeSet';
-
+import UserProfile from "./pages/profile/UserProfile";
 
 function App() {
   return (
     <div className="App">
-       <HomeSet></HomeSet>
+      <HomeSet />
     </div>
   );
 }
--- frontend/src/HomeSet.js
+++ frontend/src/HomeSet.js
@@ -3,10 +3,13 @@ import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 import Cards from "../../components/cards/Cards";
 import Login from "../login/Login";
+
 export default function HomeSet() {
     return (
         <div>
+
             <Navbar></Navbar>
+            <Hero />
             <Cards></Cards>
             <Footer></Footer>
         </div>
