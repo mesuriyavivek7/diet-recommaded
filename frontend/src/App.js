544574131460980a906d5c7c0951a4c17a05ab1c
Harshilshah11 <shahharshil054@gmail.com>
2024-06-29 14:58:57 +0530

    Changes Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
 
+import './App.css';
+
 import HomeSet from './pages/home/HomeSet';
-import Footer from './components/footer/Footer';
-import Cards from './components/cards/Cards';
-import Navbar from './components/navbar/Navbar';
-import SignUpForm from "./components/form/SignUpForm";
-import AddProfile from "./components/form/AddProfile";
+
 
 function App() {
-  return (<>
-
+  return (
     <div className="App">
-      {/* <HomeSet></HomeSet> */}
-      {/* <Navbar/>
-        <Cards/>
-        <Footer/> */}
+       <HomeSet></HomeSet>
     </div>
-  </>
   );
 }
 
