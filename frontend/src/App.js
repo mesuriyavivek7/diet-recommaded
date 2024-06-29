e3eb5a9b164f73d61bafadff31b1a1e062b4e59e
Harshilshah11 <shahharshil054@gmail.com>
2024-06-29 10:21:36 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import HomeSet from './pages/home/HomeSet';
 
 function App() {
-  return (
+  return (<>
+  <p>lorem 100</p>
     <div className="App">
         <HomeSet></HomeSet>
+
     </div>
+  </>
   );
 }
 
