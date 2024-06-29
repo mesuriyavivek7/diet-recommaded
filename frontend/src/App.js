24d6e07e8a6e15cfe87092f5420b40208d5318b0
Harshilshah11 <shahharshil054@gmail.com>
2024-06-29 13:12:56 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 import HomeSet from './pages/home/HomeSet';
 import Footer from './components/footer/Footer';
 import Cards from './components/cards/Cards';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
         {/* <HomeSet></HomeSet> */}
+        <Navbar/>
         <Cards/>
         <Footer/>
     </div>
